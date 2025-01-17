a659e4bc936d03a451d9fd5212aa05ae6924b982
tyy-tou <tyuyang300@gmail.com>
2025-01-17 19:15:30 +0800

    Add files via upload

---
+++ work_01.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int* intersec(int a[],int n1,int b[],int n2,int s[])
+{
+	int *p=(int*)malloc(sizeof(int)*n1),*q=(int*)malloc(sizeof(int)*n2);
+	scanf("%d",&n1);
+	for(p=a;p<a+n1;p++)
+	{
+		scanf("%d",p);
+	}
+	scanf("%d",&n2);
+	for(q=a;q<b+n2;q++)
+	{
+		scanf("%d",q);
+	}
+	
+}
+int main()
+{
+	int a[20];
+	int b[20];
+	int s[40];
+
+return 0;
+}
